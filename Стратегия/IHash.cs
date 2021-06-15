6973efaa6096b505357cdf3ee633dfc55e058d77
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-06-15 21:54:15 +0300

    Паттерн стратегия. Не самый удачный пример, но разобрался.

---
+++ Стратегия/IHash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Стратегия
+{
+    public interface IHash
+    {
+        string GetHash(User user);
+    }
+}
---
+++ Стратегия/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Стратегия
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() { Name = "Dima", Email = "dima@mail.ru", Password = "123456" };
+            user.HashStrategy = new PasswordHash();
+            Console.WriteLine(user.HashStrategy.GetHash(user));
+
+            user.HashStrategy = new EmailPasswordHash();
+            Console.WriteLine(user.HashStrategy.GetHash(user));
+
+            Console.ReadKey();
+        }
+    }
+}
