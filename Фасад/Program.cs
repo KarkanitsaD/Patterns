b6e3de71d746d28e46a42e6084c7dec415a30064
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-06-22 15:05:44 +0300

    Паттерн фасад - сложное сделать простым.

---
+++ Фасад/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фасад
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHouse smartHouse = new SmartHouse();
+            smartHouse.StartMorning();
+
+            Console.ReadKey();
+        }
+    }   
+
+    class SmartHouse
+    {
+        Multicooker multicooker = new Multicooker();
+        RecordPlayer recordPlayer = new RecordPlayer();
+        Toilet toilet = new Toilet();
+        Bathroom bathroom = new Bathroom();
+        public void StartMorning()
+        {
+            recordPlayer.PlayMorningMusic();
+            toilet.LightOn();
+            multicooker.CookBreakfast();
+            bathroom.FillBathroomWithCoolWater();
+        }
+    }
+
+    class Multicooker
+    {
+        public void CookBreakfast()
+        {
+            Console.WriteLine("Мультиварка готовит на завтрак кашу.");
+        }
+    }
+
+
+    class RecordPlayer
+    {
+        public void PlayMorningMusic()
+        {
+            Console.WriteLine("Магнитофон играет музыку из леса.");
+        }
+    }
+
+    class Toilet
+    {
+        public void LightOn()
+        {
+            Console.WriteLine("Свет в туалете включился.");
+        }
+    }
+
+    class Bathroom
+    {
+        public void FillBathroomWithCoolWater()
+        {
+            Console.WriteLine("Ванная начала наполняться холодной водой.");
+        } 
+    }
+
+
+}
